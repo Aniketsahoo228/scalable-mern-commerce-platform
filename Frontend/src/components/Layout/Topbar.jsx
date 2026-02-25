7e9f51844023540d23dcb9f2d871177d883fad02
Aniketsahoo228 <23051003@kiit.ac.in>
2026-02-25 23:39:54 +0530

    Refine topbar layout update

--- Frontend/src/components/Layout/Topbar.jsx
+++ Frontend/src/components/Layout/Topbar.jsx
@@ -5,7 +5,7 @@ import {RiTwitterXLine} from "react-icons/ri"
 const Topbar = () => {
   return <div className="bg-[#ea2e0e] text-white">
       <div className="container mx-auto flex justify-between items-center py-3 px-4">
-        <div class = "flex items-center space-x-4">
+        <div class = "hidden md:flex items-center space-x-4">
             <a href="#" className = "hover:text-gray-300">
               <TbBrandMeta className="h-5 w-5"/>
             </a>
@@ -16,11 +16,13 @@ const Topbar = () => {
               <RiTwitterXLine className="h-4 w-4"/>
             </a>
         </div>
-        <div className="text-sm text-center">
+        <div className="text-sm text-center flex-grow">
             <span>We ship worldwide - Fast and reliable shipping!</span>
         </div>
-        <div className="text-sm">
-            <a href="tel:+1234567890" className ="hover:text-gray-300"></a>
+        <div className="text-sm hidden md:block">
+            <a href="tel:+1234567890" className ="hover:text-gray-300">
+                +1 (234) 567-890
+            </a>
         </div>
       </div>
   </div>
