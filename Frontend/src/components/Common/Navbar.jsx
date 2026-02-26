a04fa2f55901152726762beea006e5b194007890
Aniketsahoo228 <23051003@kiit.ac.in>
2026-02-26 20:45:29 +0530

    Add navbar and tailwind color theme updates

---
+++ Frontend/src/components/Common/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <>
+    <nav className = "container mx-auto flex item-center justify-betweem py-4 px-6">
+        {
+            <div>
+                <link to = "/" className = "text-2xl font-medium">
+                AURELLE
+                </link>
+            </div>
+        }
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
--- Frontend/src/components/Layout/Topbar.jsx
+++ Frontend/src/components/Layout/Topbar.jsx
@@ -3,9 +3,9 @@ import {IoLogoInstagram} from "react-icons/io"
 import {RiTwitterXLine} from "react-icons/ri"
 
 const Topbar = () => {
-  return <div className="bg-[#ea2e0e] text-white">
+  return <div className="bg-rabbit-red text-white">
       <div className="container mx-auto flex justify-between items-center py-3 px-4">
-        <div class = "hidden md:flex items-center space-x-4">
+        <div className = "hidden md:flex items-center space-x-4">
             <a href="#" className = "hover:text-gray-300">
               <TbBrandMeta className="h-5 w-5"/>
             </a>
