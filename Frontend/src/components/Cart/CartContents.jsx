512bfdb150f708640ba6429dba5a3e480ca63b4f
Aniketsahoo228 <23051003@kiit.ac.in>
2026-02-27 23:42:37 +0530

    Update cart drawer and add cart contents

---
+++ Frontend/src/components/Cart/CartContents.jsx
@@ -0,0 +1,15 @@
+const CartContents = () => {
+  const cartProducts = [
+    {
+        productId : 1,
+        name :"T-shirt",
+        size : "M",
+        color : "red",
+        quantity : 1,
+        price : 15,
+        image : "",
+    },
+  ]
+}
+
+export default CartContents
