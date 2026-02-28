8fb84e7e00a297387e8126f9ebb31ca815583b5c
Aniketsahoo228 <23051003@kiit.ac.in>
2026-02-28 23:27:43 +0530

    Add mobile nav drawer toggle and close icon

--- Frontend/src/components/Common/Navbar.jsx
+++ Frontend/src/components/Common/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { HiOutlineShoppingBag, HiOutlineUser } from "react-icons/hi";
 import { HiBars3BottomRight } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
 import { useState } from "react";
@@ -54,13 +55,27 @@ const Navbar = () => {
 
             </div>
 
-              <button className = "md:hidden"> 
+              <button onClick={toggleNavDrawer} className = "md:hidden"> 
                 <HiBars3BottomRight className = "h-6 w-6 text-gray-700" />
               </button>
             </div>  
     </nav>
     <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+   
+    <div
+      className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full 
+      bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+        navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavDrawer}>
+            <IoMdClose className="h-6 w-6 text-gray-600" />
+          </button>
+        </div>
+      </div>
     </>
+    
   )
 }
 
