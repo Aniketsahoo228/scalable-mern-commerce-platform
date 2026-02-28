ee255608153c60a6f046b27ccefa6ccb8280057c
Aniketsahoo228 <23051003@kiit.ac.in>
2026-02-28 23:49:10 +0530

    Add footer component and update user layout

---
+++ Frontend/src/components/Common/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return(
+  <footer className="border-t py-12">
+    <div className="container ,x-auto grid grid-col-1 md:grid-col-4 gap-4 lg:px-0">
+        <div>
+            <h3 className = "text-lg text-gray-800 mb-4">NewsLettter</h3>
+            <p className="text-gray-500 mb-4">
+                Be the first to hear about new products, exclusive events, and online offers.
+            </p>
+            <p className="text-gray-500 mb-4">
+               Sig
+            </p>
+        </div>
+    </div>
+  </footer>
+);
+}
+
+export default Footer
