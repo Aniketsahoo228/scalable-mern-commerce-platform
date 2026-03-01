52b053e2169d3f740c2603e26166c52bd97f0d5d
Aniket Sahoo <23051003@kiit.ac.in>
2026-03-01 20:47:09 +0530

    Add home product sections and update layout

---
+++ Frontend/src/components/Products/NewArrivals.jsx
@@ -0,0 +1,132 @@
+const NewArrivals = () => {
+
+  const newArrivals = [
+    {
+      _id: "1",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "2",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "3",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "4",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "5",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "6",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "7",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "8",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "9",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+    {
+      _id: "10",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
+        },
+      ],
+    },
+  ];
+  
+
+  return (
+  <section>
+  <div className="container mx-auto text-center mb-10 relative">
+    <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
+    <p className="text-lg text-gray-600 mb-8">
+      Discover the latest styles straight off the runway, freshly added to keep your wardrobe on the cutting edge of fashion.
+    </p>
+    <div className="absolute right-0 ">
+
+    </div>
+  </div>
+</section>
+ );
+};
+
+export default NewArrivals;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Layout/Hero";
+import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+
+const Home = () => {
+  return (
+    <divb>
+    <Hero />
+    <GenderCollectionSection />
+    </divb>
+  );
+};
+
+export default Home;
