cdd044bdf923055d0ef83bee349a479cade56e31
Aniket Sahoo <23051003@kiit.ac.in>
2026-03-03 22:38:53 +0530

    Update checkout component

--- Frontend/src/components/Cart/Checkout.jsx
+++ Frontend/src/components/Cart/Checkout.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const cart = {
   products: [
@@ -21,6 +22,8 @@ const cart = {
 };
 
 const Checkout = () => {
+  const navigate = useNavigate();
+  const [checkoutId, setCheckoutId] = useState(null)
   const [shippingAddress, setShippingAddress] = useState({
     firstName: "",
     lastName: "",
@@ -31,12 +34,16 @@ const Checkout = () => {
     phone: "",
   });
 
+  const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    setCheckoutId(123)
+  }
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
       {/* Left Section */}
       <div className="bg-white rounded-lg p-6">
         <h2 className="text-2xl uppercase mb-6">Checkout</h2>
-        <form>
+        <form onSubmit={handleCreateCheckout}> 
             <h3 className="text-lg mb-4">Contact Details</h3>
             <div className="mb-4">
                 <label className="block text-gray-700">Email</label>
@@ -89,7 +96,7 @@ const Checkout = () => {
                     })
                 }
                     className = "w-full p-2 border rounded"
-                    requiresd                />
+                    required              />
             </div>
             <div className="mb-4 grid grid-cols-2 gap-4">
                 
@@ -124,32 +131,43 @@ const Checkout = () => {
             
             </div>
              <div className="mb-4">
-                <label className="block text-gray-700">Address</label>
+                <label className="block text-gray-700">Country</label>
                 <input 
                 type = "text"
-                value={shippingAddress.address}
+                value={shippingAddress.country}
                 onChange={(e) =>
                     setShippingAddress({
                       ...shippingAddress,
-                     address : e.target.value,
+                     country : e.target.value,
                     })
                 }
                     className = "w-full p-2 border rounded"
-                    requiresd                />
+                    required                />
             </div>
              <div className="mb-4">
-                <label className="block text-gray-700">Address</label>
+                <label className="block text-gray-700">Phone</label>
                 <input 
-                type = "text"
-                value={shippingAddress.address}
+                type = "tel"
+                value={shippingAddress.phone}
                 onChange={(e) =>
                     setShippingAddress({
                       ...shippingAddress,
-                     address : e.target.value,
+                     phone : e.target.value,
                     })
                 }
                     className = "w-full p-2 border rounded"
-                    requiresd                />
+                    required               
+                     />
+            </div>
+            <div className="mt-6">
+                {!checkoutId ?(
+                    <button type="submit" className="w-full bg-black text-white py-3 rounded">Continue The Payment</button>
+                ):(
+                    <div>
+                        <h3 className="text-lg mb-4">Pay with Paypal</h3>
+                    </div> 
+                )}
+
             </div>
         </form>
       </div>
