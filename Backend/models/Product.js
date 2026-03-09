11fe45e947ecfc404931b4a07d9db3b95fa3d264
Aniket Sahoo <23051003@kiit.ac.in>
2026-03-09 22:45:34 +0530

    Refine product model

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -17,5 +17,63 @@ const productSchema = new mongoose.Schema({
         type : Number,
         required: true,
         default: 0,
-    }
-})
+    },
+    sku : {
+        type: String,
+        unique: true,
+        required : true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+    },
+    sizes: {
+        type: [String],
+        required: true,
+    },
+    colors: {
+        type: [String],
+        required: true,
+    },
+    collections: {
+        type: String,
+        required: true,
+    },
+    material: {
+        type: String,
+    },
+    gender: {
+        type: String,
+        enum: ["Men", "Women", "Unisex"],
+    },
+
+    images: [
+    {
+        url: {
+        type: String,
+        required: true,
+        },
+        altText: {
+        type: String,
+        },
+    },
+    ],
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+
+    isPublished: {
+        type: Boolean,
+        default: false,
+    },
+
+    rating: {
+        type: Number,
+        default: 0,
+    },
+  }
+);
