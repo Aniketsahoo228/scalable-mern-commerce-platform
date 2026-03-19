bdf6c7221eba3caeb8bf9c3e67f9300b89f3fdc8
Aniket Sahoo <23051003@kiit.ac.in>
2026-03-19 22:10:46 +0530

    Fix product route syntax issues

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -4,39 +4,45 @@ const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// @route GET /api/products
-// @desc Get a single product ID
-// @access Public
-router.get("/:id", async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    return res.status(200).json(product);
-
-  } catch (error) {
-    console.error("Error fetching product:", error);
-
-    if (error.name === "CastError") {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    return res.status(500).json({ message: "Server Error" });
-  }
-});
-
 // @route GET /api/products/similar/:id
 // @desc Retrieve similar products based on the current product's gender and category
 // @access Public
 router.get("/similar/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    console.log(id);
+    const product = await Product.findById(id);
+
+    if (!product){
+      return res.status(404).json({message : "Product not found"});
+    }
+    const similarProducts = await Product.find({
+      _id: { $ne: id },
+      gender: product.gender,
+      category: product.category,
+    }).limit(4);
+
+    res.json(similarProducts);
   } catch (error) {
-    
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route GET /api/products/best-seller
+// @desc Retrieve product with highest rating
+// @access Public
+router.get("/best-seller", async (req, res) => {
+  try {
+    const bestSeller = await Product.findOne().sort({ rating: -1 });
+
+    if (bestSeller) {
+      res.json(bestSeller);
+    } else {
+      res.status(404).json({ message: "No best seller found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error")
   }
 });
 
@@ -131,6 +137,23 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.error("Error fetching products:", error);
     return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route GET /api/products/new-arrivals
+// @desc Retrieve latest 8 products - Creation date
+// @access Public
+router.get("/new-arrivals", async (req, res) => {
+  try {
+    // Fetch latest 8 products
+    const newArrivals = await Product.find()
+      .sort({ createdAt: -1 })
+      .limit(8);
+
+    res.json(newArrivals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
   }
 });
 
